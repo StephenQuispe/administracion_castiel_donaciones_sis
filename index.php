6980fb46e97c893b6bdadd8344910ff181d95ff3
Stephen Quispe <michaelstephenquispelipa@gmail.com>
2025-01-26 16:44:58 -0400

    index añadido

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donantes</title>
+    <!-- Enlace a Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <h1 class="text-center mb-4">FUNDACION CASTIEL - ADMINISTRACION</h1>
+    </header>
+    <div>
+        <ul>
+            <li><a href="">Configuracion</a></li>
+            <li><a href="">Gestion de Donaciones</a></li>
+            <li><a href="">Gestion de Campañas</a></li>
+            <li><a href="">Gestion de Noticias</a></li>
+            <li><a href="">Gestion de Voluntarios</a></li>
+            <li><a href="">Gestion de Donantes</a></li>
+            <li><a href="">Gestion de Beneficiarios</a></li>
+            <li><a href="">Gestion de Usuarios</a></li>
+        </ul>
+    </div>
+    <div>
+        <p>BIENVENIDO</p>
+    </div>
+    <!-- Enlace a Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
